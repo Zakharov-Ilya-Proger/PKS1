a46a436249f63e81a8c5c469f7f2eec2f5464969
Iiya Zakharov <2146376@gmail.com>
2024-12-16 22:37:44 +0300

    PKS 13

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -41,140 +41,144 @@ class _HomePageState extends State<HomePage> {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Обновить или удалить анализ'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: _titleController,
-                decoration: const InputDecoration(
-                  labelText: 'Название',
-                ),
-              ),
-              TextField(
-                controller: _daysController,
-                decoration: const InputDecoration(
-                  labelText: 'Дни',
-                ),
-              ),
-              TextField(
-                controller: _costController,
-                decoration: const InputDecoration(
-                  labelText: 'Стоимость',
-                ),
-                keyboardType: TextInputType.number,
-              ),
-              DropdownButton<String>(
-                value: _selectedType,
-                onChanged: (String? newValue) {
-                  if (newValue != null) {
-                    setState(() {
-                      _selectedType = newValue;
-                    });
-                  }
-                },
-                items: <String>['кровь', 'кал', 'моча']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Отмена'),
-            ),
-            TextButton(
-              onPressed: () async {
-                final title = _titleController.text;
-                final days = _daysController.text;
-                final cost = int.tryParse(_costController.text) ?? 0;
-
-                if (title.isNotEmpty && days.isNotEmpty && cost > 0) {
-                  final updatedItem = Analyze(
-                    id: item?.id ?? DateTime.now().millisecondsSinceEpoch,
-                    title: title,
-                    cost: cost,
-                    days: days,
-                    type: _selectedType,
-                    favorite: item?.favorite ?? false,
-                  );
-
-                  try {
-                    if (item != null) {
-                      await ApiService().updateProduct(updatedItem);
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Элемент успешно обновлен!'),
-                          duration: Duration(seconds: 3),
-                        ),
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return AlertDialog(
+              title: const Text('Обновить или удалить анализ'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: _titleController,
+                    decoration: const InputDecoration(
+                      labelText: 'Название',
+                    ),
+                  ),
+                  TextField(
+                    controller: _daysController,
+                    decoration: const InputDecoration(
+                      labelText: 'Дни',
+                    ),
+                  ),
+                  TextField(
+                    controller: _costController,
+                    decoration: const InputDecoration(
+                      labelText: 'Стоимость',
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                  DropdownButton<String>(
+                    value: _selectedType,
+                    onChanged: (String? newValue) {
+                      if (newValue != null) {
+                        setState(() {
+                          _selectedType = newValue;
+                        });
+                      }
+                    },
+                    items: <String>['кровь', 'кал', 'моча']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
                       );
+                    }).toList(),
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Отмена'),
+                ),
+                TextButton(
+                  onPressed: () async {
+                    final title = _titleController.text;
+                    final days = _daysController.text;
+                    final cost = int.tryParse(_costController.text) ?? 0;
+
+                    if (title.isNotEmpty && days.isNotEmpty && cost > 0) {
+                      final updatedItem = Analyze(
+                        id: item?.id ?? DateTime.now().millisecondsSinceEpoch,
+                        title: title,
+                        cost: cost,
+                        days: days,
+                        type: _selectedType,
+                        favorite: item?.favorite ?? false,
+                      );
+
+                      try {
+                        if (item != null) {
+                          await ApiService().updateProduct(updatedItem);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Элемент успешно обновлен!'),
+                              duration: Duration(seconds: 3),
+                            ),
+                          );
+                        } else {
+                          await ApiService().addProductToServer(updatedItem);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Элемент успешно добавлен!'),
+                              duration: Duration(seconds: 3),
+                            ),
+                          );
+                        }
+                        _titleController.clear();
+                        _daysController.clear();
+                        _costController.clear();
+                        Navigator.of(context).pop();
+                        _loadProducts();
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Ошибка обновления элемента: $e'),
+                            duration: const Duration(seconds: 3),
+                          ),
+                        );
+                      }
                     } else {
-                      await ApiService().addProductToServer(updatedItem);
                       ScaffoldMessenger.of(context).showSnackBar(
                         const SnackBar(
-                          content: Text('Элемент успешно добавлен!'),
+                          content: Text('Пожалуйста, заполните все поля'),
                           duration: Duration(seconds: 3),
                         ),
                       );
                     }
-                    _titleController.clear();
-                    _daysController.clear();
-                    _costController.clear();
-                    Navigator.of(context).pop();
-                    _loadProducts(); // Перезапрашиваем данные с сервера
-                  } catch (e) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text('Ошибка обновления элемента: $e'),
-                        duration: const Duration(seconds: 3),
-                      ),
-                    );
-                  }
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Пожалуйста, заполните все поля'),
-                      duration: Duration(seconds: 3),
-                    ),
-                  );
-                }
-              },
-              child: const Text('Обновить или добавить'),
-            ),
-            TextButton(
-              onPressed: () async {
-                if (item != null) {
-                  try {
-                    await ApiService().deleteProduct(item.id);
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Элемент успешно удален!'),
-                        duration: Duration(seconds: 3),
-                      ),
-                    );
-                    Navigator.of(context).pop();
-                    _loadProducts(); // Перезапрашиваем данные с сервера
-                  } catch (e) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text('Ошибка удаления элемента: $e'),
-                        duration: const Duration(seconds: 3),
-                      ),
-                    );
-                  }
-                }
-              },
-              child: const Text('Удалить'),
-            ),
-          ],
+                  },
+                  child: const Text('Обновить или добавить'),
+                ),
+                TextButton(
+                  onPressed: () async {
+                    if (item != null) {
+                      try {
+                        await ApiService().deleteProduct(item.id);
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Элемент успешно удален!'),
+                            duration: Duration(seconds: 3),
+                          ),
+                        );
+                        Navigator.of(context).pop();
+                        _loadProducts(); // Перезапрашиваем данные с сервера
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Ошибка удаления элемента: $e'),
+                            duration: const Duration(seconds: 3),
+                          ),
+                        );
+                      }
+                    }
+                  },
+                  child: const Text('Удалить'),
+                ),
+              ],
+            );
+          },
         );
       },
     );
